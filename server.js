c1e41893c73ddf2621844495b00d3db5c115c66a
Stephanie Blom <stephblom@hotmail.com>
2020-12-14 10:29:49 -0700

    make it better

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require( "express" );
 const bodyParser = require('body-parser');
 const mysql = require( 'mysql' );
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 class Database {
